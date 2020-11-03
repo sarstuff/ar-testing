20e37ebe6dd20b931b589f524a7c9da686cfa2d2
Mick Desmond <mdesmond@liquid-computing.co.uk>
2020-11-03 16:59:07 +0000

    6

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function renderPlaces(places) {
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         // model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
         model.setAttribute('rotation', '0 180 0');
-        model.setAttribute(`value', '${name}`);
+        model.setAttribute('value', `${name}`);
         model.setAttribute('scale', '0.5 0.5 0.5');
 
         model.addEventListener('loaded', () => {
