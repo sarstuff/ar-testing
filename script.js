b0c2484d27f17bfd7b21ad068563c1c84bcc02aa
Mick Desmond <mdesmond@liquid-computing.co.uk>
2020-11-03 17:42:27 +0000

    trying distance

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function staticLoadPlaces() {
     return [
         {
             name: 'Foot Team 1',
-            color: 'white',
+            color: 'green',
             location: {
                 lat: 51.226055,
                 lng: 0.89673259,
@@ -37,6 +37,7 @@ function renderPlaces(places) {
 
     places.forEach((place) => {
 
+
         let latitude = place.location.lat;
         let longitude = place.location.lng;
         let color = place.color;
@@ -44,8 +45,15 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-text');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+
+        let distanceMsg = model.getAttribute('distanceMsg');
+        console.log(distanceMsg);
+
+
+
+
         model.setAttribute('color', `${color}`);
-        model.setAttribute('value', `${name}`);
+        model.setAttribute('value', `${name} ${distanceMsg}`);
         model.setAttribute('scale', '50 50 50');
 
         model.addEventListener('loaded', () => {
